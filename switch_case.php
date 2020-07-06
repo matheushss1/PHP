04a9ced3537a92ef2028331e30cbdd4a9573d8db
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-07-06 13:30:48 -0300

    correção

--- switch_case.php
+++ switch_case.php
@@ -1,29 +1,37 @@
 <?php
 
-echo "Que dia é hoje?";
+echo "Que dia é hoje? R: ";
 
-$diaDaSemana = date('w');
+$diaDaSemana = date("w");
 
 switch ($diaDaSemana) {
 
     case 0:
         echo "Domingo";
+    break;
 
     case 1:
         echo "Segunda";
+    break;
+
     case 2:
         echo "Terça";
+    break;
 
     case 3:
         echo "Quarta";
-    
+    break;
     case 4:
        echo "Quinta";
+    break;
+
     case 5:
         echo "Sexta";
+    break;
+
     case 6:
         echo "Sábado";
-    
+    break;
 }
 
 
